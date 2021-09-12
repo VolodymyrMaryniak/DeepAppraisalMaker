d69a2b61502b575f939247799004bb7de9d1b4ab
BogdanChernikov <bogdan.chernikov15@gmail.com>
2021-09-12 20:47:18 +0300

    Add IsActive property, add migration

--- Data/Configurations/QuizEntityConfiguration.cs
+++ Data/Configurations/QuizEntityConfiguration.cs
@@ -4,20 +4,21 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AspNetCoreVueStarter.Data.Configurations
 {
-	public class QuizEntityConfiguration : IEntityTypeConfiguration<QuizEntity>
-	{
-		public void Configure(EntityTypeBuilder<QuizEntity> builder)
-		{
-			builder.ToTable("Quiz");
+    public class QuizEntityConfiguration : IEntityTypeConfiguration<QuizEntity>
+    {
+        public void Configure(EntityTypeBuilder<QuizEntity> builder)
+        {
+            builder.ToTable("Quiz");
 
-			builder.Property(x => x.Id).IsRequired();
-			builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.IsActive).IsRequired();
 
-			builder.Property(x => x.CreatedDate).IsRequired();
-			builder.Property(x => x.ModifiedDate).IsRequired();
+            builder.Property(x => x.CreatedDate).IsRequired();
+            builder.Property(x => x.ModifiedDate).IsRequired();
 
-			builder.HasMany(x => x.Questions)
-				.WithOne(x => x.Quiz);
-		}
-	}
+            builder.HasMany(x => x.Questions)
+                .WithOne(x => x.Quiz);
+        }
+    }
 }
